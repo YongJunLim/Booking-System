49bfd9d4eae0e3eeda5bbed079b286c0b0a9602a
DHSYongJun <lim.yongjun@dhs.sg>
2020-03-12 22:39:25 +0800

    test hash func for ref code

---
+++ test_hashing.py
@@ -0,0 +1,9 @@
+from hashlib import sha3_512
+from secrets import choice
+
+def generateRefCode(ID):
+    digest = sha3_512(ID.encode('utf-8')).hexdigest().upper()
+    refCode = ''.join(choice(digest) for i in range(6))
+    return refCode
+
+print(generateRefCode('000001'))
